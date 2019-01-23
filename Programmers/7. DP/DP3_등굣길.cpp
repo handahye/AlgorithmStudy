62e5cdb905f21f0591beccd9ca6141b4ae8871b1
handahye <handahye2@naver.com>
2019-01-23 16:05:18 +0900

    Update DP3_등굣길.cpp

--- Programmers/7. DP/DP3_등굣길.cpp
+++ Programmers/7. DP/DP3_등굣길.cpp
@@ -2,15 +2,14 @@
 #include <vector>
 #include <queue>
 using namespace std;
-int dist[100][100];
+int dist[101][101];
 int solution(int m, int n, vector<vector<int>> puddles) {
 	memset(dist, 0, sizeof(dist));
 	for (int i = 0; i<puddles.size(); i++)
 		dist[puddles[i][0]][puddles[i][1]] = -1; //¹° ¿õµ¢ÀÌ
-	dist[1][1] = 1;
-	for (int i = 1; i <= m + 1; i++) {
-		for (int j = 1; j <= n + 1; j++) {
-			if (i == 1 && j == 1) continue;
+	dist[1][0] = 1;
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j <= n; j++) {
 			if (dist[i][j] == -1) {
 				dist[i][j] = 0;
 				continue;
