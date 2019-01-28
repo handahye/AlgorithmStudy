a7e1924f62696406a635bb6da2c569462b85bbaf
handahye <handahye2@naver.com>
2019-01-28 14:13:17 +0900

    Update Greedy7_저울.cpp

--- Programmers/6. Greedy/Greedy7_저울.cpp
+++ Programmers/6. Greedy/Greedy7_저울.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int solution(vector<int> &weight) {
-	int num = 1;//가장 작은 추의 무게가 1이니까 맨 처음 만들 수 있는 무게는 1
+int solution(vector<int> weight) {
+	int sum = 1;//가장 작은 추의 무게가 1이니까 맨 처음 만들 수 있는 무게는 1
 	sort(weight.begin(), weight.end());
 	for (int i = 0; i<weight.size(); i++) {
-		if (num<weight[i]) break;//추가하려는 추의 무게가 현재까지 만들 수 있는 무게보다 커지는 경우
-		num += weight[i]; //weight[i]를 하나씩 추가하는 경우 1~num까지의 무게를 만들 수 있음
+		if (sum < weight[i]) break;//현재 1~sum까지의 무게를 만들 수 있는데 최대 무게인 sum보다 추의 무게가 커지면 안됨
+		num += weight[i]; //현재 sum에 추의 무게를 추가함 
 	}
 	return num;
 }
